47296c0660a3781f76a59be1952adf27c1b90cb1
Đeparminhtet® <dpmh999@outlook.com>
2026-05-08 02:42:01 +0630

    default

--- test.php
+++ test.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Test</title>
+    <link rel="stylesheet" href="/bootstrap-5.3.7-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/fontawesome-free-7.0.0-web/css/all.css">
 </head>
 <body>
     <?php
