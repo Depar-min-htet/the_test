5c7233ca16097d7393c51745ec86dfbdd7ed28b9
Đeparminhtet® <dpmh999@outlook.com>
2026-05-08 06:04:39 +0630

    "Default 1.1"

--- basic.php
+++ basic.php
@@ -1,3 +1,18 @@
 <?php
-  echo "Hello PHP";  
-?>
+
+// $name = "Deparminhtet";
+// $role = "Web Developer";
+// $company = "Fair Way Technology";
+// echo "$name is a $role at $company.";
+
+//Escape
+// $Dollor = "100";
+// echo "I only have \$$Dollor.";
+
+//Single Quote
+// $name = 'Depar';
+// echo 'Welcome $name.';
+
+
+
+
