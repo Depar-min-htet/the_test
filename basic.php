241b3a5475ccc8f8d2c5a00ce866ac5158920a2a
Đeparminhtet® <dpmh999@outlook.com>
2026-05-07 23:54:12 +0630

    "5/7/26"

--- basic.php
+++ basic.php
@@ -1,2 +1,18 @@
-<?php
-declare(strict_types=1);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello " . date("Y-m-d") . " Php";
+    echo " <> ";
+    ?>
+
+</body>
+
+</html>
