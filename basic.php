5e89ef17cb7abac800778ae3b1fefe93d8038145
Đeparminhtet® <dpmh999@outlook.com>
2026-05-08 06:27:18 +0630

    "Default_1.2.0"

--- basic.php
+++ basic.php
@@ -1,18 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basic</title>
+    <link rel="stylesheet" href="/bootstrap-5.3.7-dist/css/bootstrap.min.css">
+</head>
+<body class="bg-secondary">
+    <?php
 
-// $name = "Deparminhtet";
-// $role = "Web Developer";
-// $company = "Fair Way Technology";
-// echo "$name is a $role at $company.";
+        // $name = "Deparminhtet";
+        // $role = "Web Developer";
+        // $company = "Fair Way Technology";
+        // echo "$name is a $role at $company.";
 
-//Escape
-// $Dollor = "100";
-// echo "I only have \$$Dollor.";
+        //Escape
+        // $Dollor = "100";
+        // echo "I only have \$$Dollor.";
 
-//Single Quote
-// $name = 'Depar';
-// echo 'Welcome $name.';
+        //Single Quote
+        // $name = 'Depar';
+        // echo 'Welcome $name.';
 
+        //String Length [strlen]
+        // echo strlen("Deparminhtet");
+        // echo "<br>";
+        // echo strlen("ဒီပါမင်းထက်");
+        
+        // Substr()
+        $str = "Deparminnhtet";
+        echo substr($str,0,5);
 
-
-
+    ?>
+</body>
+</html>
